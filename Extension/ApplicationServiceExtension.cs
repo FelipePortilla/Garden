753ec2208e89e7bdc446597fd83405c905194a96
FelipePortilla <128866686+FelipePortilla@users.noreply.github.com>
2023-11-23 13:26:38 -0500

    Add files via upload

---
+++ Extension/ApplicationServiceExtension.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Extension
+{
+    public  static class ApplicationServiceExtension
+    {
+        public static void ConfigureCors(this IServiceCollection services)=>
+        services.AddCors(options =>{
+
+            options.AddPolicy("CorsPolicy", builder=>
+            builder.AllowAnyOrigin()
+            .AllowAnyOrigin()
+            .AllowAnyHeader());
+        }
+
+        );
+    }
+}
